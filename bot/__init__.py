5e6466a152d71f0fed96bb72fddf515086f1ba1b
mrotttamil <90447292+mrotttamil@users.noreply.github.com>
2022-04-30 16:18:32 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -121,23 +121,23 @@ if os.path.exists('drive_folder'):
 try:
     TITLE_NAME = getConfig('TITLE_NAME')
     if len(TITLE_NAME) == 0:
-        TITLE_NAME = 'Helios-Mirror-Search'
+        TITLE_NAME = 'MR-Mirror-Search'
 except KeyError:
-    TITLE_NAME = 'Helios-Mirror-Search'
+    TITLE_NAME = 'MR-Mirror-Search'
 
 try:
     AUTHOR_NAME = getConfig('AUTHOR_NAME')
     if len(AUTHOR_NAME) == 0:
-        AUTHOR_NAME = 'Helios-Mirror-Bot'
+        AUTHOR_NAME = 'Anonymous'
 except KeyError:
-    AUTHOR_NAME = 'Helios-Mirror-Bot'
+    AUTHOR_NAME = 'Anonymous'
 
 try:
     AUTHOR_URL = getConfig('AUTHOR_URL')
     if len(AUTHOR_URL) == 0:
-        AUTHOR_URL = 'https://t.me/heliosmirror'
+        AUTHOR_URL = 'https://t.me/RafiqCreationz'
 except KeyError:
-    AUTHOR_URL = 'https://t.me/heliosmirror'
+    AUTHOR_URL = 'https://t.me/RafiqCreationz'
 
 
 updater = tg.Updater(token=BOT_TOKEN,use_context=True)
